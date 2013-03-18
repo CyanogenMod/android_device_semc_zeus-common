f6f8c9be0d5779f3cab2933bb4c73281fa52aad2
Michael Bestas <mikeioannina@gmail.com>
2013-03-18 22:37:44 +0200

    zeus-common: remove filler
    Bootloader bug is fixed now
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ifdfc5314b711a723bd86c4b403889de51a8c4b47

--- zeus.mk
+++ zeus.mk
@@ -25,7 +25,6 @@ PRODUCT_COPY_FILES += \
 
 # Init files
 PRODUCT_COPY_FILES += \
-    device/semc/msm7x30-common/prebuilt/filler:root/filler2 \
     device/semc/zeus-common/prebuilt/init.semc.rc:root/init.semc.rc
 
 
